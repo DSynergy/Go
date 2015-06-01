326e52e008b2e9a00b5feb450051e7c0ff6cf08a
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-01 14:32:46 -0600

    tests are setup in mocha

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var expect = chai.expect;
 
 describe('Test the board', function(){
   it('exists', function() {
-    expect($('#board')).to.be.ok;
-  } )
+    var board = $('#board');
+    expect(board.exists()).to.be.ok;
+  });
 });
