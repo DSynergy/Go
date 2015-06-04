378021b609370b0be123ace7b3995240681e5a14
Dustin Gault <Dustin.Gault@gmail.com>
2015-06-04 16:04:31 -0600

    add some debugging ability and add logic to reverse X,Y in relation to nested array

--- src/board.js
+++ src/board.js
@@ -29,23 +29,44 @@ Board.prototype.update = function(x, y) {
     return false
   }
   if (this.currentPlayer === this.BLACK) {
-      this.grid[x][y] = this.BLACK;
+      this.setValue(x,y,this.BLACK);
       this.currentPlayer = this.WHITE;
       return true;
   } else {
-      this.grid[x][y] = this.WHITE;
+      this.setValue(x,y,this.WHITE);
       this.currentPlayer = this.BLACK;
       return true;
   }
 };
 
 Board.prototype.isValidMove = function(x, y) {
-  return this.grid[x][y] == this.EMPTY;
+  return this.get(x,y) == this.EMPTY;
 };
 
 Board.prototype.hasLiberty = function(x,y) {
-
+  var color = this.get(x,y);
+  if (this.neighborValues(x,y).indexOf("empty") > -1)
+    return true;
+  else if (this.neighborValues(x,y).indexOf(color) > -1) {
+    return "pizza";
+  } else {
+    return false;
+  }
 }
+
+Board.prototype.get = function(x,y) {
+  return this.grid[y][x];
+}
+
+Board.prototype.setValue = function(x,y,value) {
+  this.grid[y][x] = value;
+}
+
+Board.prototype.neighborValues = function(x,y) {
+  return this.neighbors(x,y).map(function(coords) {
+    return this.get.apply(this, coords);
+  }.bind(this));
+};
 
 Board.prototype.neighbors = function(x,y) {
   var points =  [[x, y - 1],
@@ -56,6 +77,21 @@ Board.prototype.neighbors = function(x,y) {
   return points.filter(function(coords) {
     return coords[0] >= 0 && coords[1] >= 0 && coords[1] < this.size && coords[0] < this.size;
   }.bind(this));
+}
+
+Board.prototype.log = function() {
+  var str = this.grid.map(function(r) {
+    return r.map(function(s) {
+      if(s == 'empty') {
+        return "X"
+      } else if (s == "white") {
+        return "W"
+      } else if (s == "black") {
+        return "B"
+      }
+    }).join("");
+  }).join("\n")
+  console.log(str);
 }
 
 
--- test/test.js
+++ test/test.js
@@ -37,7 +37,7 @@ describe('The Board', function() {
     it('should update an intersection', function() {
         var black = board.BLACK;
         board.update(0,0);
-        expect(board.grid[0][0]).to.be.eql(black);
+        expect(board.get(0,0)).to.be.eql(black);
     });
 
     it('should update to white on second move', function() {
@@ -46,9 +46,9 @@ describe('The Board', function() {
         board.update(0,0);
         board.update(1,1);
         board.update(2,2);
-        expect(board.grid[0][0]).to.be.eql(black);
-        expect(board.grid[1][1]).to.be.eql(white);
-        expect(board.grid[2][2]).to.be.eql(black);
+        expect(board.get(0,0)).to.be.eql(black);
+        expect(board.get(1,1)).to.be.eql(white);
+        expect(board.get(2,2)).to.be.eql(black);
     });
 
     it('cant play on top of another already played intersection', function() {
@@ -57,7 +57,7 @@ describe('The Board', function() {
         expect(board.currentPlayer).to.be.eql(black);
         var validMove = board.update(2,2);
         var invalidMove = board.update(2,2);
-        expect(board.grid[2][2]).to.be.eql(black);
+        expect(board.get(2,2)).to.be.eql(black);
         expect(validMove).to.be.eql(true);
         expect(invalidMove).to.be.eql(false);
         expect(board.currentPlayer).to.be.eql(white);
@@ -78,8 +78,9 @@ describe('The Board', function() {
       expect(board.liberties(1,2)).to.be.eql(6);
     });
 
-    xit('determines if an intersection has liberty', function() {
+    it('determines if an intersection has liberty', function() {
       board.update(1,1);
+      board.log();
       expect(board.hasLiberty(1,1)).to.be.eql(true);
       board.update(10,10);
       board.update(1,2);
@@ -91,11 +92,15 @@ describe('The Board', function() {
       expect(board.neighbors(1,1)).to.be.eql([[1,0],[2,1],[1,2],[0,1]]);
     });
 
+    it("finds neighbor values at coord", function() {
+      expect(board.neighborValues(1,1)).to.be.eql(["empty", "empty", "empty", "empty"]);
+    });
+
     it("finds neighbors at edges", function() {
       expect(board.neighbors(0,0)).to.be.eql([[1,0],[0,1]]);
     });
 
-    it('tracks narrowing liberty', function() {
+    xit('tracks narrowing liberty', function() {
       moveSeq = [[0,1],[1,1],[1,0],[10,10],[1,2],[6,6]];
       moveSeq.forEach(function(coords) {
         board.update.apply(board, coords);
@@ -105,6 +110,32 @@ describe('The Board', function() {
       expect(board.hasLiberty(1,1)).not.to.be.ok;
     });
 
+    xit('tracks narrowing liberty', function() {
+      moveSeq = [[0,1]//b
+                //  [1,1],//w
+                //  [1,0],//b
+                //  [2,1],//w
+                //  [0,2],//b
+                //  [1,2],//w
+                //  [2,2],//b
+                //  [8,8],//w
+                //  [1,3]//b
+                 ];
+      moveSeq.forEach(function(coords) {
+        board.update.apply(board, coords);
+      });
+
+      board.log();
+      // expect(board.hasLiberty(1,1)).to.be.ok;
+      // board.update(2,1);
+      // expect(board.hasLiberty(1,1)).not.to.be.ok;
+    });
+
+    //XBX
+    //BWB
+    //BWB
+    //XXX
+    //
 
     //XBX
     //BWB
@@ -115,6 +146,6 @@ describe('The Board', function() {
       moveSeq.forEach(function(coords) {
         board.update.apply(board, coords);
       });
-      expect(board.grid[1][1]).to.be.eql("empty")
+      expect(board.get(1,1)).to.be.eql("empty")
     });
 });
